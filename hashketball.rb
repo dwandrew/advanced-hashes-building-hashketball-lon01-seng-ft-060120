e3e2e7d19e4def263f4c26e66d07f65b02a7424c
Daniel Andrew <dwandrew@hotmail.com>
2020-04-07 07:33:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
       :steals =>3,
       :blocks =>1,
       :slam_dunks =>1
-    },{
+    }, {
       :player_name =>'Reggie Evans',
       :number =>30,
       :shoe =>14,
@@ -25,7 +25,7 @@ def game_hash
       :steals =>12,
       :blocks =>12,
       :slam_dunks =>7
-    },{
+    }, {
       :player_name => 'Brook Lopez',
       :number =>11,
       :shoe =>,17
@@ -35,7 +35,7 @@ def game_hash
       :steals =>3,
       :blocks =>1,
       :slam_dunks => 15
-    },{
+    }, {
       :player_name =>'Mason Plumlee',
       :number =>1,
       :shoe =>19,
@@ -45,7 +45,7 @@ def game_hash
       :steals =>3,
       :blocks =>8,
       :slam_dunks =>5
-    },{
+    }, {
       :player_name => 'Jason Terry',
       :number =>31,
       :shoe =>15,
