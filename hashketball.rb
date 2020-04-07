0c9bbd5f2f41c67e8441ec655017b5aa6ff24483
Daniel Andrew <dwandrew@hotmail.com>
2020-04-07 07:31:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,13 +4,115 @@ def game_hash
   :home =>{
     :team_name => 'Brooklyn Nets',
     :colors => ['Black', 'White'],
-    :players => {}
+    :players => {
+      {
+      :player_name =>'Alan Anderson',
+      :number => 0,
+      :shoe => 16,
+      :points =>22,
+      :rebounds =>12,
+      :assists =>12,
+      :steals =>3,
+      :blocks =>1,
+      :slam_dunks =>1
+    },{
+      :player_name =>'Reggie Evans',
+      :number =>30,
+      :shoe =>14,
+      :points =>12,
+      :rebounds =>12,
+      :assists =>12,
+      :steals =>12,
+      :blocks =>12,
+      :slam_dunks =>7
+    },{
+      :player_name => 'Brook Lopez',
+      :number =>11,
+      :shoe =>,17
+      :points =>17,
+      :rebounds =>19,
+      :assists =>10,
+      :steals =>3,
+      :blocks =>1,
+      :slam_dunks => 15
+    },{
+      :player_name =>'Mason Plumlee',
+      :number =>1,
+      :shoe =>19,
+      :points =>26,
+      :rebounds =>11,
+      :assists =>6,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    },{
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    }
+    }
   },
     
   :away => {
     :team_name => 'Charlotte Hornets',
     :colors => ['Turquoise', 'Purple'],
-    :players => {}
+    :players => {{
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    },{
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    },{
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    },{
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    },{
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    }}
   }
   }
 end
